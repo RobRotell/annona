1bac09d3f36c4f210e74ef0bbad4859d6c3daa7b
Rob <dev@robrotell.com>
2024-03-03 18:06:56 -0500

    Setting up Vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+import path from 'path'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		preact(),
+		ViteMinifyPlugin({}),
+	],
+	build: {
+		target: browserslistToEsbuild(),
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve( __dirname, 'src' ),
+		},
+	},
+})
