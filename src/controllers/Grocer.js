d4f52fde2b7a6162a16a6a33f4ec577b1c4fd4f1
Rob <dev@robrotell.com>
2024-03-06 22:11:31 -0500

    Building out methods to add/delete items

--- src/controllers/Grocer.js
+++ src/controllers/Grocer.js
@@ -8,23 +8,26 @@ export class Grocer {
 	/**
 	 * Fetch grocery items
 	 *
+	 * @todo currently fetching creds twice (once in this method and once in API); see if there's a way to optimize
+	 *
 	 * @return {Promise<array>}
 	 */
-	static async getItems() {
-		const creds = await Creds.getCreds()
+	static getItems() {
+		return new Promise( resolve => {
+			Creds
+				.getCreds()
+				.then( creds => {
 
-		// if user isn't logged in, return empty list
-		if( !creds ) {
-			return []
-		}
+					// if user isn't logged in, return empty list
+					if( !creds ) {
+						return resolve( [] )
+					}
 
-		const req = await API.sendRequest(
-			'items/get',
-			'GET',
-			true,
-		)
-
-		return req?.items ?? []
+					API
+						.sendRequest( 'items/get', 'GET', true )
+						.then( res => resolve( res?.items ?? [] ) )
+				})
+		})
 	}
 
 
@@ -32,10 +35,37 @@ export class Grocer {
 	 * Add grocery item
 	 *
 	 * @param {string} name
-	 * @return {bool} True, if successful
+	 * @return {Promise<number>} New item ID, if successful
 	 */
 	static addItem( name ) {
+		return new Promise( ( resolve, reject ) => {
+			Creds
+				.getCreds()
+				.then( creds => {
 
+					// don't save if user isn't logged in
+					if( !creds ) {
+						return reject( 'You must be logged in to save items.' )
+					}
+
+					API
+						.sendRequest( 'items/add', 'POST', true, {
+							name
+						}).then( res => {
+
+							// if no status prop, something went wrong with API
+							if( !res.status ) {
+								return reject( 'Failed to save item. Please try again.' )
+							} else {
+								if( 'success' === res.status ) {
+									return resolve( res.items[0].id )
+								} else {
+									return reject( res.message )
+								}
+							}
+						})
+				})
+		})
 	}
 
 
@@ -45,7 +75,7 @@ export class Grocer {
 	 * @param {number} id
 	 * @param {string} name
 	 *
-	 * @return {bool} True, if successful
+	 * @return {boolean} True, if successful
 	 */
 	static updateItem( id, name ) {
 
@@ -55,11 +85,34 @@ export class Grocer {
 	/**
 	 * Delete grocery item
 	 *
+	 * @todo currently fetching creds twice (once in this method and once in API); see if there's a way to optimize
+	 *
 	 * @param {number} id
-	 * @return {bool} True, if successful
+	 * @return {Promise<boolean>} True, if successful
 	 */
 	static deleteItem( id ) {
+		return new Promise( ( resolve, reject ) => {
+			Creds
+				.getCreds()
+				.then( creds => {
 
+					// if user isn't signed in, just delete locally
+					if( !creds ) {
+						return resolve( true )
+					}
+
+					API
+						.sendRequest( 'items/delete', 'DELETE', true, {
+							id
+						}).then( res => {
+							if( res.status && 'success' === res.status ) {
+								return resolve( true )
+							} else {
+								return reject( false )
+							}
+						})
+				})
+		})
 	}
 
 }
