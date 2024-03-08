833680cd6fc521edb1abe99bc84956a0653bd46f
Rob <dev@robrotell.com>
2024-03-07 22:44:47 -0500

    Adding and configuring PWA support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 import path from 'path'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
@@ -10,6 +11,42 @@ export default defineConfig({
 	plugins: [
 		preact(),
 		ViteMinifyPlugin({}),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'inline',
+			workbox: {
+				sourcemap: true,
+			},
+			manifest: {
+				name: 'Annona',
+				short_name: 'Annona',
+				description: 'Grocery shopping. Simplified.',
+				theme_color: '#FFFF99',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		})
 	],
 	build: {
 		target: browserslistToEsbuild(),
